3b4e8f806a299700905390a2ee74dec876bdbe8b
Jack Gallagher <jack@gallabytes.com>
2016-04-28 20:01:50 -0700

    Clean data.reflection
    Autoquoters turned out to be a dead end in the presence of type classes.
    This would be a problem for performance, except that I figured a way to
    get around the problems with pattern matching on names.

--- library/data/reflection.lean
+++ library/data/reflection.lean
@@ -37,71 +37,6 @@ namespace lean
     definition expr.eq_dec [instance] : ∀ (e1 e2 : expr), decidable (e1 = e2) := sorry
     definition level.eq_dec [instance] : ∀ (l1 l2 : level), decidable (l1 = l2) := sorry
 
-    namespace expr
-      definition arity : expr → ℕ
-      | (pi _ A B) := 1 + arity B
-      | _ := 0
-
-      definition arglength (α : name → ℕ) : expr → ℕ
-      | (var i) := 0
-      | (const nm _) := α nm
-      | (loc nm t) := arity t
-      | (meta nm t) := arity t
-      | (app f x) := arglength f - 1
-      | (lam _ _ b) := arglength b + 1
-      | _ := 0
-
-      namespace autoquote
-        -- an attempt at formalizing autoquoters as seen in Agda reflection
-        inductive qexp : ℕ → Type₁ := -- simple exprs tagged with arity
-        | fvr : ∀ a, name → qexp a -- free var w/o type, `con`
-        | tvr : ∀ t, name → qexp (arity t) -- free var w/type, `loc` and `meta`
-        | app : ∀{n}, qexp (n + 1) → qexp 0 → qexp n -- should use vecs like in Agda, but Lean is bad at inductives
-        | def : ∀{n}, qexp n -- default value
-  
-        private definition tag (α : name → ℕ) : ∀ e, qexp (arglength α e)
-        | (const nm l) := qexp.fvr (α nm) nm
-        | (loc nm t) := qexp.tvr t nm
-        | (meta nm t) := qexp.tvr t nm
-        | (app f x) := if arglength α f = 0 then qexp.def -- this whole thing needs to be cleaned
-                       else if arglength α x = 0
-                            then qexp.app (cast sorry (tag f)) (cast sorry (tag x)) -- thanks to UIP, this computes
-                            else qexp.def
-        | e := qexp.def
-
-        structure table (A : Type) :=
-          mk :: (vars : list (name × syntax.expr))
-                (nmap : hlist (λ nme, nary (arity (pr₂ nme)) A A) vars)
-                (fvars : list (name × ℕ))
-                (fvmap : hlist (λ nme, nary (pr₂ nme) A A) fvars)
-
-        private definition convert (A : Type) (θ : table A) {n : ℕ} (e : qexp n) : option (nary n A A) :=
-          qexp.rec_on e
-            (λ a nm, dite ((nm,a) ∈ table.fvars θ)
-                          (λ v, option.some (hlist.get (table.fvmap θ) v))
-                          (λ nv, option.none))
-            (λ t nm, dite ((nm,t) ∈ table.vars θ)
-                          (λ v, option.some (hlist.get (table.nmap θ) v))
-                          (λ nv, option.none))
-          (λ n f x f' x', by eapply option.monad.ap; apply f'; apply x') -- why this works and not the other I don't know
-          (λ n, option.none)
-
-        private definition lookup {A B : Type} [H : decidable_eq A] (a : A) : list (A × B) → option B
-        | [] := option.none
-        | ((k,v) :: xs) := if a = k then option.some v else lookup xs
-
-        private abbreviation tagFromTable {A : Type} (θ : table A) (nm : name) : ℕ :=
-          option.rec_on (lookup nm (table.fvars θ)) 0 id
-
-        abbreviation convertManages {A : Type} (θ : table A) (e : expr) : Prop :=
-          match convert A θ (tag (tagFromTable θ) e) with (option.some t) := true | option.none := false end
-
-        definition doConvert {A : Type} (θ : table A) (e : expr) :
-          ∀ {thm : convertManages θ e}, nary (arglength (tagFromTable θ) e) A A :=
-          option.rec_on (convert A θ (tag (tagFromTable θ) e)) (false.rec _) (λ t thm, t)
-      end autoquote
-    end expr
-
     namespace varops
       open expr
       section
@@ -117,6 +52,7 @@ namespace lean
 
       private abbreviation wk_var (n d ix : nat) : expr :=
         if d ≤ ix then var (n + ix) else var ix
+
       definition wk := bvop ∘ wk_var
 
       private abbreviation instantiate_var (vs : list expr) (d ix : nat) : expr :=
